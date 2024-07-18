8584888aeee0de5e801128f6a5e4cd69e2a1c784
Tarun Sharma <83560354+sharmatarun392000@users.noreply.github.com>
2024-07-18 12:40:03 +0000

    DataType Summary

---
+++ .devcontainer/01_Basics/DATA-TYPES-SUMMARY.js
@@ -0,0 +1,17 @@
+// Primitive data types :- String, Number, Boolean, null, undefined, Symbol, BigInt
+const bigNum=34973847n // decleration and initialization of BigInt
+// Javascript is a dynamically typed language
+
+// Reference Types(Non primitive) :- Array, Objects, Functions
+const heros = ["shaktiman", "doga"]
+let myobj = {
+    name: "hitesth" ,
+    age: 22,
+}
+
+const myFunction= function()
+{
+    console.log("Hello world")
+}
+
+myFunction();

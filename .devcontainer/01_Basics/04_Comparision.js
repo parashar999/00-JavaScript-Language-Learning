97c213167393779fb549c03dc0e0af1f3d57f703
Tarun Sharma <83560354+sharmatarun392000@users.noreply.github.com>
2024-07-18 12:27:22 +0000

    Learn : Comparisions

---
+++ .devcontainer/01_Basics/04_Comparision.js
@@ -0,0 +1,11 @@
+// 
+console.log("2" > 1); // automatic conversion of two to number
+
+console.log(null>0); // false
+console.log(null==0); // false
+console.log(null>=0); // true Which is not true 
+
+// === strict check , check value and data type also 
+
+console.log("2"===2) // false
+console.log("2"==2) // true

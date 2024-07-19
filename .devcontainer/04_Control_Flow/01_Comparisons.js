0080b1f61c0ef05504016fa4615d0add088614ed
Tarun Sharma <83560354+sharmatarun392000@users.noreply.github.com>
2024-07-19 12:39:51 +0000

    Learn : Comparisions , If else statement

---
+++ .devcontainer/04_Control_Flow/01_Comparisons.js
@@ -0,0 +1,19 @@
+
+    if(41)
+    {
+    
+    }
+    else if(91)
+    {
+
+    }
+    else
+    {
+    
+    }
+    
+    // > , < , <=, >= , == , != , ===, !==
+
+    // short notation
+
+    if( 500 )  console.log(100); //code

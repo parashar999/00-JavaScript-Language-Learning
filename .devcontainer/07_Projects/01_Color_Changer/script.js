04c06b094bdc7023b057b4586d3c81dad45f08ca
sharmatarun392000 <parashartarun392000@gmail.com>
2024-07-20 23:33:23 +0530

    IMPLEMENT : COLOR CHANGE PROJECT

---
+++ .devcontainer/07_Projects/01_Color_Changer/script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach((button)=> 
+    {
+        button.addEventListener('click',(event)=>
+        {
+            console.log(event)
+            console.log(event.target)
+
+            if(event.target.id==='grey')
+            {
+                body.style.backgroundColor=event.target.id
+            }
+            if(event.target.id==='white')
+            {
+                body.style.backgroundColor=event.target.id
+            }
+            if(event.target.id==='yellow')
+            {
+                body.style.backgroundColor=event.target.id
+            }
+            if(event.target.id==='blue')
+            {
+                body.style.backgroundColor=event.target.id
+            }
+        }
+        )
+    }
+)

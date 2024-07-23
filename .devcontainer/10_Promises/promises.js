3cacd4b0e1520f3923c300d490962f238c6d124e
Tarun Sharma <83560354+sharmatarun392000@users.noreply.github.com>
2024-07-23 21:44:25 +0000

    Learn : fetch in detail

--- .devcontainer/10_Promises/promises.js
+++ .devcontainer/10_Promises/promises.js
@@ -98,8 +98,6 @@ async function getAllUsers()
     }
 }
 getAllUsers()
-
-
 fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
     return response.json
 }).then((data)=>{console.log(data)}).catch((error)=>console.log(error))
